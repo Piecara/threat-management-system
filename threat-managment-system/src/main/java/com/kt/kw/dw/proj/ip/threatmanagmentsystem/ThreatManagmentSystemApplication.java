06ec7e4ec3f8fde9be7a782b20c19e94833d8231
dominik.wawrzkiewicz <dominik.wawrzkiewicz@bluesoft.com>
2020-06-11 14:17:17 +0200

    project base

---
+++ threat-managment-system/src/main/java/com/kt/kw/dw/proj/ip/threatmanagmentsystem/ThreatManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kt.kw.dw.proj.ip.threatmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreatManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreatManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ threat-managment-system/src/test/java/com/kt/kw/dw/proj/ip/threatmanagmentsystem/ThreatManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.kw.dw.proj.ip.threatmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreatManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
